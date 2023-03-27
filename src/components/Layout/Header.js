7bf204f8fc6921a1397f737a89444c96b4cb65ed
Piotr Regulski <bulu901@gmail.com>
2023-03-27 19:36:14 +0200

    Createin' Header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import HeaderImage from "../../assets/headerPhoto.jpg";
+import classes from "./Header.module.css";
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>TakeClothes</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={HeaderImage} alt="clothes hanging on a hanger" />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
